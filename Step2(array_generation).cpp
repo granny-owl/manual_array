1891ff8401b8b5072a9611181c907998cad0078b
granny-owl <70903146+granny-owl@users.noreply.github.com>
2020-09-15 11:33:08 +0300

    Add files via upload

---
+++ Step2(array_generation).cpp
@@ -0,0 +1,31 @@
+﻿
+// more pointers
+#include <iostream>
+
+
+int main()
+{
+	int* arr, n;
+	std::cout << "Number of elements:\n";
+	std::cin >> n;
+	arr = new int[n];
+	std::cout << "Enter elements:\n";
+	for (int i = 0; i < n; i++) {
+		std::cin >> arr[i];
+	}
+	std::cout << "Your elements:\n";
+	for (int i = 0; i < n; i++) {
+		std::cout << arr[i] << std::endl;
+	}
+	int max = arr[0], min = max;
+	for (int i = 0; i < n; i++) {
+		if (arr[i] > max) max = arr[i];
+		if (arr[i] < min) min = arr[i];
+	}
+	std::cout << "Min and max:\n";
+	std::cout << min << ", " << max;
+	delete arr;
+	return 0;
+}
+
+/*запилить ввод размера массива, его генерацию, поиск наибольшего и наименьшего элементов*/
